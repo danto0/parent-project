007ea73de060cccea47037fad0a30d574b1bfb7b
dantolino <david.antolinorivas@igt.com>
2023-05-16 18:40:30 +0200

    first

---
+++ childB/src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import com.google.common.base.Strings;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )  {
+        System.out.println(Strings.repeat("hi", 3));
+    }
+}
